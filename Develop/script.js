2614b977b5fb9e3e87a367569d0185b475f4b391
Stephanee Hayden <stephaneehayden@gmail.com>
2022-11-30 09:58:57 +1000

    code is now working

--- Develop/script.js
+++ Develop/script.js
@@ -5,54 +5,57 @@ var generateBtn = document.querySelector("#generate");
 function writePassword() {
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
-
   passwordText.value = password;
-
 }
-
-//Set Variables
-//  lowercase
-var lower = ("abcdefghiklmnopqrstuvwxyz")
-//  uppercase
-var upper = ("ABCDEFGHIJKLMNOPQRSTUVWXYZ")
-//  numeric
-var number = ("01234567890")
-//  special characters
-var special = (" !"#$%&'()*+,-./:;<=>?@[\]^_`{|}~")
-
 
 // start function to generate password
 
 function generatePassword() {
+
+  var lower = ("abcdefghiklmnopqrstuvwxyz");
+  var upper = ("ABCDEFGHIJKLMNOPQRSTUVWXYZ");
+  var number = ("1234567890");
+  var special = ( "!#$%&'*+,-./:;<=>?@\^_`{|}~" );
+
+  //TODO: PROMPT user inpupt
+  var inputLength = prompt("How many characters does your password need?", "Enter a number between 8 and 128");
   
-}
+  //  Password Length (8 - 128)
+  if(inputLength >= 8 && inputLength <= 128) {
+    var passwordContent = ("");
+    var password = ("");
+    console.log("inputLength =" + inputLength); //TODO: remove later
 
+  } else { //failure call if password length not met
+    window.alert("Password must be between 8 and 128 characters");
+    console.log("Password is not long enough" + inputLength); //TODO: remove later
+    generatePassword(); }
 
+    //prompts for specific characters
+    if(confirm("Does your password need to include lowercase characters?")) {
+      passwordContent = passwordContent + lower; };
+          
+    if(confirm("Does your password need to include uppercase characters?")) {
+      passwordContent = passwordContent + upper; };
+    
+    if(confirm("Does your password need to include numbers?")) {
+      passwordContent = passwordContent + number; };
+    
+    if(confirm("Does your password need to include special characters?")) {
+      passwordContent = passwordContent + special; };
+    
+    if(passwordContent==""){ //if user selects no for all options, no string is generated
+      window.alert("Password must contain some characters - please try again");
+      console.log("Password does not contain any characters" + passwordContent); //TODO: remove later
+      generatePassword(); //goes back to beginning of function
+    }; 
 
-
-//TODO: PROMPT user inpupt
-//Criteria: 
-//  Password Length (8 - 128)
-var minLength = 8;
-var maxLength = 128;
-//Prompt for user input - how many characters
-var inputLength = prompt("How many characters does your password need?", "Enter a number between 8 and 128")
-
-if (inputLength < minLength || inputLength > maxLength ) {
-  alert("Please enter a valid number between 8 and 128"); }
-    else {
-      return;
-    }
-
-
-
-
-// TODO: At least one of the above options must be true
-// Loop back to start if criteria is not met
-
-// TODO: Password is displayed in an alert or written to the page
-
-
+      // make password be a random result of above options based on above results
+      for (let i = 0; i < inputLength; i++) {
+        password = password + passwordContent[(Math.floor(Math.random() * passwordContent.length))];
+      };
+        return password;
+      }
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);

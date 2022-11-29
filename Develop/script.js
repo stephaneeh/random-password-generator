b051f7e995b9e550817e95b9200f00ed79efa2e7
Stephanee Hayden <stephaneehayden@gmail.com>
2022-11-29 10:22:01 +1000

    set password variable parameters

--- Develop/script.js
+++ Develop/script.js
@@ -1,11 +1,6 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
-
-//TODO: PROMPT user inpupt
-//
-
-
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
@@ -14,6 +9,50 @@ function writePassword() {
   passwordText.value = password;
 
 }
+
+//Set Variables
+//  lowercase
+var lower = ("abcdefghiklmnopqrstuvwxyz")
+//  uppercase
+var upper = ("ABCDEFGHIJKLMNOPQRSTUVWXYZ")
+//  numeric
+var number = ("01234567890")
+//  special characters
+var special = (" !"#$%&'()*+,-./:;<=>?@[\]^_`{|}~")
+
+
+// start function to generate password
+
+function generatePassword() {
+  
+}
+
+
+
+
+//TODO: PROMPT user inpupt
+//Criteria: 
+//  Password Length (8 - 128)
+var minLength = 8;
+var maxLength = 128;
+//Prompt for user input - how many characters
+var inputLength = prompt("How many characters does your password need?", "Enter a number between 8 and 128")
+
+if (inputLength < minLength || inputLength > maxLength ) {
+  alert("Please enter a valid number between 8 and 128"); }
+    else {
+      return;
+    }
+
+
+
+
+// TODO: At least one of the above options must be true
+// Loop back to start if criteria is not met
+
+// TODO: Password is displayed in an alert or written to the page
+
+
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);

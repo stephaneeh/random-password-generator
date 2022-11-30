d31b969dd1226f8524e858c3f680a148620dd8cc
Stephanee Hayden <stephaneehayden@gmail.com>
2022-11-30 10:08:49 +1000

    removed unnecessary code and updated comments

--- Develop/script.js
+++ Develop/script.js
@@ -9,7 +9,6 @@ function writePassword() {
 }
 
 // start function to generate password
-
 function generatePassword() {
 
   var lower = ("abcdefghiklmnopqrstuvwxyz");
@@ -24,12 +23,11 @@ function generatePassword() {
   if(inputLength >= 8 && inputLength <= 128) {
     var passwordContent = ("");
     var password = ("");
-    console.log("inputLength =" + inputLength); //TODO: remove later
-
-  } else { //failure call if password length not met
+  } 
+  //failure call if password length not met
+  else { 
     window.alert("Password must be between 8 and 128 characters");
-    console.log("Password is not long enough" + inputLength); //TODO: remove later
-    generatePassword(); }
+    generatePassword(); } //returns to beginning of function
 
     //prompts for specific characters
     if(confirm("Does your password need to include lowercase characters?")) {
@@ -44,17 +42,19 @@ function generatePassword() {
     if(confirm("Does your password need to include special characters?")) {
       passwordContent = passwordContent + special; };
     
-    if(passwordContent==""){ //if user selects no for all options, no string is generated
+      //if user selects no for all options, no string is generated - return to beginning of function
+    if(passwordContent==""){ 
       window.alert("Password must contain some characters - please try again");
-      console.log("Password does not contain any characters" + passwordContent); //TODO: remove later
-      generatePassword(); //goes back to beginning of function
+      generatePassword();
     }; 
 
-      // make password be a random result of above options based on above results
+      // randomise password content based on true results above and inputLength
       for (let i = 0; i < inputLength; i++) {
         password = password + passwordContent[(Math.floor(Math.random() * passwordContent.length))];
       };
-        return password;
+
+      return password;
+        
       }
 
 // Add event listener to generate button
